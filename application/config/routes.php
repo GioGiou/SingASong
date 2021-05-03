f48a0e9395a6b3c3a50fd2f4068720658b40b4bb
jani <jani.suban@gmail.com>
2021-05-03 12:10:37 +0200

    Dodatne informacije o glasbeniku

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,6 +51,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 */
+$route['glasbenik'] = 'glasbeniki/index';
+$route['glasbenik/(:any)'] = 'glasbeniki/view/$1';
 $route['assets/(:any)'] = 'assets/$1';
 $route['user_authentication/signup'] = 'User_authentication/signup';
 $route['(:any)'] = 'welcome/index';
--- application/views/event/view.php
+++ application/views/event/view.php
@@ -1,5 +1,6 @@
 <div>
-    <h3><?php echo $news_item['Naslov']; ?></h3>
-    <p><?php echo $news_item['Kraj']; ?>, <?php echo $news_item['Datum']; ?></p>
+    <img src="<?php echo asset_url();?>photos/<?php echo $photo;?>">
+    <h3><?php echo $news_item['Ime']; ?></h3>
+    <!--<p><?php echo $news_item['Kraj']; ?>, <?php echo $news_item['Datum']; ?></p>-->
     <p><?php echo $news_item['Opis']; ?></p>
 </div>
