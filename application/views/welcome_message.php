3d18bab41b0727e460fabf4c730309bc8dc407dc
GioGiou <46958087+GioGiou@users.noreply.github.com>
2021-05-31 13:52:19 +0200

    Update welcome_message.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,4 +1,4 @@
-<div>
+<div class="group">
     <div><a href="<?php echo site_url('glasbenik'); ?>">Prikaži glasbenike</a></div>
     <div><a href="<?php echo site_url('user_authentication/login_form'); ?>">Prijava</a></div>
 </div>
