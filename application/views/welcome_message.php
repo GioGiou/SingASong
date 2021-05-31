e6ede944ad435a5e76e7ae2234a40309dffecbb5
GioGiou <46958087+GioGiou@users.noreply.github.com>
2021-05-31 14:15:36 +0200

    Update welcome_message.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,4 +1,4 @@
 <div class="group">
-    <div><a id="buttonPrijava" href="<?php echo site_url('glasbenik'); ?>">Prikaži glasbenike</a></div>
-    <div><a  id="buttonPrijava" href="<?php echo site_url('user_authentication/login_form'); ?>">Prijava</a></div>
+    <div><a href="<?php echo site_url('glasbenik'); ?>">Prikaži glasbenike</a></div>
+    <div><a href="<?php echo site_url('user_authentication/login_form'); ?>">Prijava</a></div>
 </div>
