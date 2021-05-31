b6c7f6420de617086164ce1756c0747a7acffe31
GioGiou <46958087+GioGiou@users.noreply.github.com>
2021-05-31 19:20:16 +0200

    Icon

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,7 +2,7 @@
         <head>
                 <title><?php if(isset($title)) echo $title; ?></title>
                 <link rel="stylesheet" href="<?php echo asset_url();?>css/style.css">
-                <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+                <link rel="icon"  href="<?php echo site_url(); ?>"><img src="<?php echo asset_url();?>icons/Logo.png">
         </head>
         <body>		        		
                 <div class="header">
