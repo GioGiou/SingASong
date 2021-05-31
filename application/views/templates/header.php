7295c2f6574695b8ea5483b52328479823bbe6b0
GioGiou <46958087+GioGiou@users.noreply.github.com>
2021-05-31 19:22:15 +0200

    Update header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,7 +2,7 @@
         <head>
                 <title><?php if(isset($title)) echo $title; ?></title>
                 <link rel="stylesheet" href="<?php echo asset_url();?>css/style.css">
-                <link rel="icon"  href="<?php echo site_url(); ?>"><img src="<?php echo asset_url();?>icons/Logo.png">
+                <link rel="icon"  href="<?php echo asset_url();?>icons/Logo.png">
         </head>
         <body>		        		
                 <div class="header">
